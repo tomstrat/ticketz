c86a11ca50dfd6da59ec94a5609e3c1d9cfdf364
Tom Strat Laptop <tomstratfic@hotmail.com>
2020-11-12 16:23:37 +0000

    updated users

--- routes/admin/users.js
+++ routes/admin/users.js
@@ -3,6 +3,9 @@ const usersPage = require("../../views/admin/users");
 const editUsersForm = require("../../views/admin/edituser");
 const { handleErrors, requireAuth, checkSelf, checkUserExists } = require("../middlewares");
 const validators = require("../validators");
+const { User, Ticket } = require("../../sequelize");
+const hasher = require("../../utilities/hasher");
+const user = require('../../models/user');
 
 
 
@@ -19,9 +22,7 @@ module.exports = (app, DB) => {
   } = validators(DB)
 
   app.get("/users", requireAuth(["admin"]), (req, res) => {
-    DB.getAllUsers((data) => {
-      res.send(usersPage({ users: data }))
-    });
+    User.findAll().then(users => res.send(usersPage({ users })));
   });
 
   app.get('/users/new', requireAuth(["admin"]), (req, res) => {
@@ -34,9 +35,8 @@ module.exports = (app, DB) => {
     checkSelf, checkUserExists(DB), (req, res) => {
       //CANT EDIT SELF
 
-      DB.getUser(req.params.username, (data) => {
-        res.send(editUsersForm({ errors: "", data }));
-      });
+      User.findOne({ where: { username: req.params.username } })
+        .then(data => res.send(editUsersForm({ errors: "", data })));
 
     });
 
@@ -55,14 +55,15 @@ module.exports = (app, DB) => {
 
       //Check Username Change
       if (req.body.username !== req.params.username) {
-        DB.updateUser({ field: "username", data: req.body.username, username: req.params.username });
+        User.update({ username: req.body.username }, { where: { username: req.params.username } });
       }
       //Check Password Change
       if (req.body.password !== "") {
-        DB.updateUser({ field: "password", data: req.body.password, username: req.body.username });
+        const hashed = hasher(req.body.password);
+        User.update({ password: hashed }, { where: { username: req.params.username } });
       }
       //Update Role
-      DB.updateUser({ field: "role", data: req.body.role, username: req.body.username });
+      User.update({ role: req.body.role }, { where: { username: req.params.username } });
 
       res.redirect("/users");
 
@@ -73,10 +74,8 @@ module.exports = (app, DB) => {
     requireAuth(["admin"]),
     checkSelf, checkUserExists(DB), (req, res) => {
       //CANT DELETE SELF
-
-      DB.deleteUser(req.params.username, () => {
-        res.redirect("/users");
-      });
+      User.destroy({ where: { username: req.params.username } })
+        .then(user => res.redirect("/users"));
 
     });
 
@@ -89,10 +88,8 @@ module.exports = (app, DB) => {
     ], handleErrors(addUserForm), requireAuth(["admin"]), (req, res) => {
       const { username, password, role } = req.body;
 
-
-      DB.createUser({ username, password, role }, (id) => {
-        res.redirect("/users");
-      });
+      User.create({ username, password, role })
+        .then(user => res.redirect("/users"));
 
     });
 };
